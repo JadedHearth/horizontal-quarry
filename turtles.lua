00a4427768aa3ef550475cae5efbd51635aad66d
Mz <mzmud@icloud.com>
2021-08-29 15:38:40 -0500

    redid fueling system, fixed issue #2
    redid fueling system to remove references to the refuel() function, and replaced it with my own system, and *actually* fixed issue #2 this time

--- turtles.lua
+++ turtles.lua
@@ -1,13 +1,15 @@
--- Derived from the Excavate turtle built-in program, made for stripmining (computercraft)
+-- Heavily modified form of the Excavate turtle built-in program, made for making large holes at a certain layer underground
 -- Created by StarNinga
 
 local name = os.getComputerLabel()
-if type(name) == nil then
-	os.setComputerLabel("miner"..math.random(0, 1000)) -- change this to being assigned by the central computer once the rednet system is in place
+if name == nil then
+	name = "miner"..math.random(0, 1000)
+	os.setComputerLabel(name) -- change this to being assigned by the central computer once the rednet system is in place
 end
 
+-- not fully configurable yet, as the fueling system assumes it requires a maximum of 2 coal to go to the chest and back
 local mineWidth = 20
-local mineLength = 40 -- to be removed once I get the wireless deactivation/activation in place
+local mineLength = 40
 
 local unloaded = 0
 local collected = 0
@@ -17,22 +19,21 @@ local xDir,zDir = 0,1
 
 -- Filled in further down:
 local goTo 
-local refuel
 local turnRight
 local turnLeft
 local tryForwards
 
-local function unload( _bKeepOneFuelStack )
+local function unload( _KeepOneFuelStack )
 	print( name.." is unloading items..." )
 	for n=1,16 do
 		local nCount = turtle.getItemCount(n)
 		if nCount > 0 then
-			turtle.select(n)			
+			turtle.select(n)
 			local bDrop = true
-			if _bKeepOneFuelStack and turtle.refuel(0) then
+			if _KeepOneFuelStack then
 				bDrop = false
-				_bKeepOneFuelStack = false
-			end			
+				_KeepOneFuelStack = false
+			end
 			if bDrop then
 				turtle.drop()
 				unloaded = unloaded + nCount
@@ -47,18 +48,9 @@ local function returnSupplies()
 	local x,z,xd,zd = xPos,zPos,xDir,zDir
 	print( name.." is returning to chest..." )
 	goTo( 0,0,0,-1 )
-	
-	local fuelNeeded = 2*(x+z) + 1
-	if not refuel( fuelNeeded ) then
-		unload( true )
-		print( name.." waiting for fuel" )
-		while not refuel( fuelNeeded ) do
-			os.pullEvent( "turtle_inventory" )
-		end
-	else
-		unload( true )	
-	end
-	
+
+	unload( true )
+
 	print( name.." is resuming mining..." )
 	goTo( x,z,xd,zd )
 end
@@ -88,53 +80,20 @@ local function collect()
 	return true
 end
 
-function refuel( amount )
-	local fuelLevel = turtle.getFuelLevel()
-	if fuelLevel == "unlimited" then
-		return true
-	end
-	
-	local needed = amount or (xPos + zPos + 2)
-	if turtle.getFuelLevel() < needed then
-		local fueled = false
-		for n=1,16 do
-			if turtle.getItemCount(n) > 0 then
-				turtle.select(n)
-				if turtle.refuel(1) then
-					while turtle.getItemCount(n) > 0 and turtle.getFuelLevel() < needed do
-						turtle.refuel(1)
-					end
-					if turtle.getFuelLevel() >= needed then
-						turtle.select(1)
-						return true
-					end
-				end
-			end
-		end
-		turtle.select(1)
-		return false
-	end
-	
-	return true
-end
-
 local function tryForwards()
-	if not refuel() then
-		print( name.." doesn't have enough fuel" )
-		returnSupplies()
-	end
 	local fuelLevel = turtle.getFuelLevel()
 	if fuelLevel <= 1 then
 		local selected = turtle.getSelectedSlot()
 		turtle.select(1)
-		turtle.refuel()
+		turtle.refuel(1)
 		turtle.select(selected)
 	end
-    turtle.digUp() 
-    turtle.digDown()
+
+	turtle.digUp() 
+	turtle.digDown()
 	while not turtle.forward() do
 		if not (turtle.attack() and turtle.attackUp() and turtle.attackDown()) then
-            turtle.dig() 
+			turtle.dig() 
 			if not collect() then
 				returnSupplies()
 			end
@@ -143,7 +102,6 @@ local function tryForwards()
 		end
 	end
 
-	
 	xPos = xPos + xDir
 	zPos = zPos + zDir
 	return true
@@ -161,9 +119,40 @@ end
 
 function goTo( x, z, xd, zd )
 
-	local goToFuelNeeded = math.abs(xPos-x)+math.abs(zPos-z)
-	if turtle.getFuelLevel() <= goToFuelNeeded then
-		refuel(goToFuelNeeded)
+	local fuelLevel = turtle.getFuelLevel()
+	if fuelLevel <= 121 then
+		local selected = turtle.getSelectedSlot()
+		turtle.select(1)
+		turtle.refuel(2)
+		turtle.select(selected)
+	end
+
+	if zPos > z then
+		while zDir ~= -1 do
+			turnLeft()
+		end
+		while zPos > z do
+			if turtle.forward() then
+				zPos = zPos - 1
+			elseif turtle.dig() or turtle.attack() then
+				collect()
+			else
+				sleep( 0.5 )
+			end
+		end
+	elseif zPos < z then
+		while zDir ~= 1 do
+			turnLeft()
+		end
+		while zPos < z do
+			if turtle.forward() then
+				zPos = zPos + 1
+			elseif turtle.dig() or turtle.attack() then
+				collect()
+			else
+				sleep( 0.5 )
+			end
+		end	
 	end
 
 	if xPos > x then
@@ -193,81 +182,44 @@ function goTo( x, z, xd, zd )
 			end
 		end
 	end
-	
-	if zPos > z then
-		while zDir ~= -1 do
-			turnLeft()
-		end
-		while zPos > z do
-			if turtle.forward() then
-				zPos = zPos - 1
-			elseif turtle.dig() or turtle.attack() then
-				collect()
-			else
-				sleep( 0.5 )
-			end
-		end
-	elseif zPos < z then
-		while zDir ~= 1 do
-			turnLeft()
-		end
-		while zPos < z do
-			if turtle.forward() then
-				zPos = zPos + 1
-			elseif turtle.dig() or turtle.attack() then
-				collect()
-			else
-				sleep( 0.5 )
-			end
-		end	
-	end
-	
+
 	while zDir ~= zd or xDir ~= xd do
 		turnLeft()
 	end
 end
 
-if not refuel() then
-	print( name.." is out of fuel" )
-	return
-end
+-- What is actually executed --
 
--- Actual Excavation bit, the hard bit --
-
-print( "Excavating..." )
-
-local alternate = 0
-local off = false
-
-while not off do
-    turnLeft()
+local fuelLevel = turtle.getFuelLevel()
+local fuelReady = turtle.getItemCount(1)
+if (fuelLevel + fuelReady * 80) > (mineWidth * mineLength + (mineWidth * mineLength * 3) / 64) * (mineWidth+mineLength) then
+	print( "Starting..." )
+	local alternate = 0
+	turnLeft()
 	for n=1,mineLength do
-        for r=1,mineWidth-1 do
-            tryForwards()
-        end
-        if alternate == 0 then
-            turnRight()
-            tryForwards()
-            turnRight()
-            alternate = 1
-        elseif alternate == 1 then
-            turnLeft()
-            tryForwards()
-            turnLeft()
-            alternate = 0
-        end
-        if off then -- to be used when I can remotely turn the miners off
-			break
+		for r=1,mineWidth-1 do
+			tryForwards()
+		end
+		if alternate == 0 then
+			turnRight()
+			tryForwards()
+			turnRight()
+			alternate = 1
+		elseif alternate == 1 then
+			turnLeft()
+			tryForwards()
+			turnLeft()
+			alternate = 0
 		end
 	end
-    off = true -- delete when I can remotely turn the miners off
+	print( name.." returning to base..." )
+
+	-- Return to where we started
+	goTo( 0,0,0,-1 )
+	unload( false )
+	goTo( 0,0,0,1 )
+
+	print( "Mined "..(collected + unloaded).." items total." )
+else
+	print("Not enough fuel to start. (Add a minimum of 39 coal in the first slot)")
 end
-
-print( name.."Returning to base..." )
-
--- Return to where we started
-goTo( 0,0,0,-1 )
-unload( false )
-goTo( 0,0,0,1 )
-
-print( "Mined "..(collected + unloaded).." items total." )
